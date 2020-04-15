d2d60e614a2054c296310fab35411c8abf785862
adipathania06 <61343011+adipathania06@users.noreply.github.com>
2020-04-15 05:45:57 -0700

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,39 @@
+class Form {
+  constructor() {
+    this.input = createInput("name");
+    this.button = createButton('play');
+    this.greeting = createElement('h2');
+  }
+   hide(){
+     this.greeting.hide();
+     this.button.hide();
+     this.greeting.hide();
+
+   }
+  display(){
+    var title = createElement('h2')
+    title.html("Car Racing Game");
+    title.position(130, 0);
+    
+    //this.input = createInput("Name");
+    //this.button = createButton('Play');
+    
+    this.input.position(130, 160);
+    this.button.position(250, 200);
+
+    this.button.mousePressed(()=>{
+     this.input.hide();
+      this.button.hide();
+
+      var name = this.input.value();
+      
+      playerCount+=1;
+      player.update(name)
+      player.updateCount(playerCount);
+      //greeting = createElement('h3');
+      this.greeting.html("Hello " + name )
+      this.greeting.position(130, 160)
+    });
+
+  }
+}
